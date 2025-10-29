c022568d1859643fee5f3250dd526c0c7e18a15d
jdroj <jdroj@JUAN_ROJAS>
2025-10-29 16:25:39 -0500

    DbContextImplementation

---
+++ src/Pacagroup.Trade.Persistence/Contexts/AplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pacagroup.Trade.Domain.Entities;
+using Pacagroup.Trade.Persistence.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Trade.Persistence.Contexts
+{
+    public class AplicationDbContext : DbContext
+    {
+        public readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public AplicationDbContext(
+            DbContextOptions<AplicationDbContext> options,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor
+        ) : base(options)
+        {
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
