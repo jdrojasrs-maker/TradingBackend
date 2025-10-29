09b71085f2fc014546c717c24a6a9245f139f76c
jdroj <jdroj@JUAN_ROJAS>
2025-10-29 16:48:12 -0500

    DependencyInjectionInfrastructure

---
+++ src/Pacagroup.Trade.Persistence/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pacagroup.Trade.Persistence.Contexts;
+using Pacagroup.Trade.Persistence.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Trade.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+            services.AddDbContext<AplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("NorthwindConnection"),
+                    b => b.MigrationsAssembly(typeof(AplicationDbContext).Assembly.FullName)
+                )
+            );
+            return services;
+        }
+    }
+}
